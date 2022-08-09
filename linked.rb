dec435110080eb233cb0b3fc9a3fa5405d03353a
absolus2 <absolus24@gmail.com>
2022-08-08 22:32:37 -0400

    add multiple methods to linkedlist

--- linked.rb
+++ linked.rb
@@ -14,26 +14,70 @@ class LinkedList
     end
   end
 
+  # append a new element at the beginning of the list
   def prepend(value)
     @head = Node.new(value, @head)
   end
 
+  # return the last element of the list
   def tail(node=@head)
     return node if node.next == nil
-    node = node.next
-    tail(node)
+    tail(node = node.next)
   end
 
+  # return the size of the list
   def size(node=@head,times=1)
     return times  if node.next == nil
-    node = node.next
-    size(node, times+1)
+    size(node = node.next, times+1)
   end
 
+  # return the list at index
   def at(index, node=@head, times=0)
     return node if times == index
-    node = node.next
-    at(index, node, times + 1)
+    at(index, node = node.next, times + 1)
+  end
+
+  # return true or false ir the value is in the list
+  def contains?(value, node=@head)
+    if node.value == value
+      return true 
+    elsif node.value != value && node.next == nil
+      return false
+    end
+    contains?(value, node = node.next)
+  end
+
+  # return the index of an element if is it in the array
+  def find(value, node=@head, times=0)
+    return times if node.value == value
+    find(value, node = node.next, times+1)
+  end
+
+  # remove the last element in the list
+  def pop(prev=nil, curr=@head)
+    return prev.next = nil if curr.next == nil
+    pop(prev = curr, curr = curr.next)
+  end
+
+  # return a string with every value in the list
+  def to_s(node=@head, array=[])
+    return array.push("nil").join("=>") if node == nil
+    array.push("(#{node.value})")
+    to_s(node.next, array)
+  end
+
+  # insert elemenet at index
+  def insert_at(value, index, prev=nil, curr=@head,times=0)
+    return prepend(value) if index == 0
+    return prev.next = Node.new(value, curr) if index == times
+    insert_at(value, index, prev = curr, curr = curr.next, times+1)
+  end
+
+  # remove element at index
+  def remove_at(index, prev=nil, curr=@head, times=0)
+    return @head = @head.next if index == 0
+    return prev.next = curr.next if index == times
+    remove_at(index, prev = curr, curr.next, times + 1)
   end
 end
 
@@ -64,4 +108,6 @@ asco.append(55)
 asco.append(88)
 asco.prepend(66)
 
+asco.remove_at(1)
+p asco
 
